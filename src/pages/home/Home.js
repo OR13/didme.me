67566638c70b6a985526d7f984d0bac0eded913b
Orie Steele <orie@transmute.industries>
2020-07-25 18:55:13 -0500

    cleanup

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -13,7 +13,7 @@ import Box from "@material-ui/core/Box";
 import { Ed25519KeyPair, driver } from "@transmute/did-key-ed25519";
 import { X25519KeyPair } from "@transmute/did-key-x25519";
 
-// import crypto from "crypto";
+import crypto from "crypto";
 import { blobToDataURL, download, updateCanvas } from "../../core";
 import { client, urlSource } from "../../core/ipfs";
 
@@ -184,10 +184,26 @@ export const Home = (props) => {
               <canvas id="meme-canvas" />
             </div>
           ) : (
-            <div style={{ textAlign: "center", padding: "32px" }}>
-              <Typography variant={"h5"} gutterBottom>
+            <div
+              style={{
+                textAlign: "center",
+                padding: "32px",
+                marginTop: "64px",
+              }}
+            >
+              <Typography variant={"h4"} gutterBottom>
                 Add an image to get started.
               </Typography>
+
+              <Typography gutterBottom>
+                Data will be published to the public IPFS network.
+                <br />
+                There is no encryption applied here, only steganography and
+                encoding.
+                <br />
+                DO NOT publish offensive content or data you do not own.
+              </Typography>
+              <br />
               <Button
                 variant={"contained"}
                 color={"secondary"}
