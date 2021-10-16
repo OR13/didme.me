6e223830c5776d1940836901f7dd1be58debef33
Orie Steele <orie@transmute.industries>
2021-10-16 11:47:02 -0500

    chore: tests

---
+++ pages/api/image.ts
@@ -0,0 +1,19 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import fs from "fs";
+import path from "path";
+
+type Data = Buffer;
+
+const filePath = path.resolve(".", "public/background-2.png");
+const imageBuffer = fs.readFileSync(filePath);
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  res.status(200);
+
+  res.setHeader("Content-Type", "image/png");
+  res.send(imageBuffer);
+}
