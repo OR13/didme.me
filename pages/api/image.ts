9748ddade60fda00fdc41d75bead9fd8cc61a59d
Orie Steele <orie@transmute.industries>
2021-10-16 11:52:17 -0500

    chore: tests

--- pages/api/image.ts
+++ pages/api/image.ts
@@ -5,7 +5,7 @@ import path from "path";
 
 type Data = Buffer;
 
-const filePath = path.resolve(".", "public/background-2.png");
+const filePath = path.resolve(".", "background-2.png");
 const imageBuffer = fs.readFileSync(filePath);
 
 export default function handler(
